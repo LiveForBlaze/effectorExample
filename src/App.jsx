8194f2738c37c1b7f661ce7666bd3ca78acbe537
Mukhamedov Rustam <rustam.mukhamedov@stecpoint.ru>
2021-04-09 14:29:37 +0300

    Стартовое состояние

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,30 @@
-import React, { useEffect } from 'react';
-import { useStore } from 'effector-react'; // добавится во время лайв кодинга
+import React from 'react';
 import { Button } from '@material-ui/core';
 import { DataListModal } from './components/DataListModal/DataListModal';
-import { setIsFetchDataVisible } from './components/DataListModal/stores/settings'; // добавится во время лайв кодинга
 import { DataList } from './components/DataList';
-import { $DataListModalSettings } from './components/DataListModal/stores/settings';
-import { fetchDataFx, $DataStore } from './components/DataList/stores/data' // добавится во время лайв кодинга
 import './App.css';
 
 function App() {
-  const { isVisible, isLoading } = useStore($DataListModalSettings); // добавится во время лайв кодинга
-  const $dataStore = useStore($DataStore); // добавится во время лайв кодинга
-
-  useEffect(() => {
-    fetchDataFx();
-  }, []);
-
   return (
     <div className="App">
       <Button
         color='primary'
         variant='contained'
-        onClick={() => setIsFetchDataVisible(true)} // добавится во время лайв кодинга, по умолчанию console.log
+        onClick={console.log}
       >
         Открыть модальное окно
       </Button>
 
       <DataListModal
-        isVisible={isVisible} // добавится во время лайв кодинга, по умолчанию false
-        isLoading={isLoading} // добавится во время лайв кодинга, по умолчанию false
-        onSubmit={fetchDataFx} // добавится во время лайв кодинга, по умолчанию console.log
-        onClose={() => setIsFetchDataVisible(false)} // добавится во время лайв кодинга, по умолчанию console.log
+        isVisible={false}
+        isLoading={false}
+        onSubmit={console.log}
+        onClose={console.log}
       />
 
       <DataList
-        isLoading={isLoading} // добавится во время лайв кодинга, по умолчанию false
-        data={$dataStore} // добавится во время лайв кодинга, по умолчанию []
+        isLoading={false}
+        data={[]}
       />
     </div>
   );
