133d96a905db4b28378431fc0cc18c1ded1c567e
Mukhamedov Rustam <rustam.mukhamedov@stecpoint.ru>
2021-04-09 11:21:33 +0300

    Добавлен лоадер к списку данных

--- src/components/DataList/index.jsx
+++ src/components/DataList/index.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect } from 'react';
 import { useStore } from 'effector-react';
+import { CircularProgress } from '@material-ui/core';
 import { fetchDataFx, $DataStore } from './stores/data';
+import { $FetchDataSettings } from '../modals/FetchData/stores/settings';
 
 export const DataList = () => {
   const $dataStore = useStore($DataStore);
+  const $fetchDataSettings = useStore($FetchDataSettings);
 
   useEffect(() => {
     fetchDataFx();
@@ -12,11 +15,15 @@ export const DataList = () => {
   return (
     <>
       {
-        $dataStore && $dataStore.map((item) => (
-          <div style={{ textAlign: 'left' }}>
-            {item.id}: {item.title}
-          </div>
-        ))
+        $fetchDataSettings.isLoading ? (
+          <CircularProgress />
+        ) : (
+          $dataStore && $dataStore.map((item) => (
+            <div style={{ textAlign: 'left' }}>
+              {item.id}: {item.title}
+            </div>
+          ))
+        )
       }
     </>
   )
